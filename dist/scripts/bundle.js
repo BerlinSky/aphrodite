7431bd474cc88538c9cffb4071ae312e0b180e72
Berlin Sky <pedroyoung8989@gmail.com>
2016-06-13 10:01:16 -0700

    Adjust the mobile menu

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -10073,42 +10073,6 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 		thisPlate.rotate(rotateDeg, 2);
 	}
-
-	// class Plate {
-	// 	constructor(plateElem) {
-	// 		this.plate = plateElem;
-	// 		this.arrowLeft = $('[data-arrow="left"]');
-	// 		this.arrowRight = $('[data-arrow="right"]');
-	// 	}
-
-	// 	_hideArrow() {
-	// 		// this.arrowLeft.fadeOut();
-	// 		this.arrowLeft.css("display", "none");
-	// 		// this.arrowRight.fadeOut();
-	// 		this.arrowRight.css("display", "none");
-	// 	}
-
-	// 	_showArrow() {
-	// 		this.arrowLeft.fadeIn(3000);
-	// 		this.arrowRight.fadeIn(3000);
-	// 	}
-
-	// 	rotate(rotateDeg, speed) {
-	// 		const transformStyle = "rotate(" + rotateDeg + "deg)";
-
-	//     console.log("transformStyle", transformStyle);
-
-	// 		this._hideArrow();
-
-	// 		this.plate.css('-webkit-transform', transformStyle);
-	// 		this.plate.css('-moz-transform', transformStyle);
-	// 		this.plate.css('transform', transformStyle);
-	// 		this.plate.css('-webkit-transition', '-webkit-transform ' + speed + 's');
-	// 		this.plate.css('transition', 'transform ' + speed + 's');
-
-	// 		this._showArrow();
-	// 	}
-	// }
 });
 
 },{"./Plate":2,"./RotationCalculator":3,"jquery":1}]},{},[4])
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -115,41 +115,5 @@ $(function () {
 
 		thisPlate.rotate(rotateDeg, 2);
 	}
-
-	// class Plate {
-	// 	constructor(plateElem) {
-	// 		this.plate = plateElem;
-	// 		this.arrowLeft = $('[data-arrow="left"]');
-	// 		this.arrowRight = $('[data-arrow="right"]');
-	// 	}
-
-	// 	_hideArrow() {
-	// 		// this.arrowLeft.fadeOut();
-	// 		this.arrowLeft.css("display", "none");
-	// 		// this.arrowRight.fadeOut();
-	// 		this.arrowRight.css("display", "none");
-	// 	}
-
-	// 	_showArrow() {
-	// 		this.arrowLeft.fadeIn(3000);
-	// 		this.arrowRight.fadeIn(3000);
-	// 	}
-
-	// 	rotate(rotateDeg, speed) {
-	// 		const transformStyle = "rotate(" + rotateDeg + "deg)";
-
-	//     console.log("transformStyle", transformStyle);
-
-	// 		this._hideArrow();
-
-	// 		this.plate.css('-webkit-transform', transformStyle); 
-	// 		this.plate.css('-moz-transform', transformStyle);
-	// 		this.plate.css('transform', transformStyle);
-	// 		this.plate.css('-webkit-transition', '-webkit-transform ' + speed + 's');
-	// 		this.plate.css('transition', 'transform ' + speed + 's');
-
-	// 		this._showArrow();
-	// 	}
-	// }
-
+	
 });
