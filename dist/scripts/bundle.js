46e7b16903f11efe878949f976001343c9ad1199
Berlin Sky <pedroyoung8989@gmail.com>
2016-06-16 18:57:05 -0700

    Set news content top position

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -10392,7 +10392,7 @@ var Plate = function () {
 	}, {
 		key: '_playGunFiringAudio',
 		value: function _playGunFiringAudio() {
-			var audio = new Audio('http://cdn.gunsnroses.com/site/Revolver_SmithWesson_RollChamber_Fienup_001.mp3');
+			var audio = new Audio('http://cdn.gunsnroses.com/site/Revolver_SmithWesson_RollChamber_Fienup_001b.mp3');
 			audio.play();
 		}
 	}, {
